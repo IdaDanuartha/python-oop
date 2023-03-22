c0874be19a9dd5b73907fdf9ff22c94f7f797dfd
IdaDanuartha <idadanuartha42@smk.belajar.id>
2023-03-22 20:37:21 +0800

    class and instance variables

--- 01-Constructor/main.py
+++ 01-Constructor/main.py
@@ -1,11 +1,17 @@
 class Hero: # template
+    # class variable
+    jumlah = 0
     def __init__(self, name, power): # constructor
         self.name = name
         self.power = power
+        Hero.jumlah += 1
 
 hero1 = Hero("Warrior", 100)
-hero2 = Hero("Archer", 150)
-
 print(hero1.name, hero1.power)
+print(Hero.jumlah)
+
+hero2 = Hero("Archer", 150)
 print(hero2.name, hero2.power)
+print(Hero.jumlah)
+
 print(hero1.__dict__, hero2.__dict__)
