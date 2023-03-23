f77426c64432516feb64fdaa157620fb71fc3ee3
IdaDanuartha <idadanuartha42@smk.belajar.id>
2023-03-23 11:23:06 +0800

    override method

---
+++ 11-OverrideMethod/main.py
@@ -0,0 +1,28 @@
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def showInfo(self):
+        print("show info di class hero")
+        print(self.name, "dengan health : ", self.health)
+
+class HeroStrength(Hero):
+    def __init__(self, name):
+        # Hero.__init__(self, name, 200)
+        super().__init__(name, 200)
+    
+    def showInfo(self):
+        print("show info di class hero strength")
+        print(self.name, "dengan health : ", self.health)
+
+class HeroIntelligence(Hero):
+    def __init__(self, name):
+        super().__init__(name, 120)
+        
+
+knight = HeroStrength("knight")
+assassin = HeroIntelligence("assassin")
+
+knight.showInfo()
+assassin.showInfo()
